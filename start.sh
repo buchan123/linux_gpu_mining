678b1e49d79060be9f4a1308c1e0ce710534d423
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2019-12-17 22:22:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  eth-asia1.nanopool.org --user 0xa252988cE525d9B84c042FbBDe4a6a6CC737ECc2 --pec --fee 0 --pass x --port 9999
+./miner --server  zec-asia1.nanopool.org --user t1aTNzYc4hm9v2Sep8NPJqxfSKqvEPuuFdx --pec --fee 0 --pass x --port 16666
